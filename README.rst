06fff44af15367b1e03b78c8e2d0157bdf1db1c7
James Gregory <james.gregory94@hotmail.co.uk>
2022-05-14 14:29:50 +0100

    final renaming

--- README.rst
+++ README.rst
@@ -1,1 +1,1 @@
-reading_gist is a package for generating chronologically ordered reading lists using a Google Scholar API.
+readily is a package for generating chronologically ordered reading lists using a Google Scholar API.
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='reading_gist',
-    version='0.0.2',
+    name='readily',
+    version='0.0.1',
     packages=find_packages()  # will return a list ['spam', 'spam.fizz']
 )
